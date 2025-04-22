8fba6b18886642ef7140421ea96d16d91d70e6de
wesley <wesleywaka77@gmail.com>
2025-04-22 20:59:42 +0300

    feat:validation and transformation

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,21 @@
+import { Controller,Get,Post,Body,Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dtos';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages(){
+
+  }
+
+  @Post()
+  createMessage(@Body() body:CreateMessageDto){
+    console.log(body);
+  }
+
+
+  @Get('/:id')
+  getMessage(@Param('id') id:string){
+    console.log(id);
+  }
+}
